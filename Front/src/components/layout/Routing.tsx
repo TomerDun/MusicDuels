1050871f798ebd7d6d434b8dea99cd6157b65bff
TomerDun <tomer@dunsky.name>
2025-09-15 20:20:53 +0300

    removed imports on routing

--- Front/src/components/layout/Routing.tsx
+++ Front/src/components/layout/Routing.tsx
@@ -1,9 +1,8 @@
 import { Route, Routes } from "react-router";
-import { Box } from "@mantine/core";
 
 export function Routing() {
     return (
-            <Routes>
+            <Routes>                
             </Routes>
     );
 }

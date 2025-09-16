d8bb7a8f3e106b8294800cec8a981c1d46b7236c
Yonatan Gorovetz <goro5357@gmail.com>
2025-09-16 15:26:52 +0300

    gameSession validation middleware

--- Back/routes/gamesRouter.ts
+++ Back/routes/gamesRouter.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import { startPractice, createSession, getSession, updateSession } from "../controllers/gamesController";
+import { gameSessionValidationSchema } from "../utils/validationSchemas/gameSessionSchema";
+import { validationHandler } from "../middlewares/validationHandler";
 
 const router = express.Router({mergeParams:true});
 
 router.get("/practice", startPractice);
-router.post("/", createSession);
+router.post("/", gameSessionValidationSchema, validationHandler, createSession);
 router.get("/:sessionId", getSession);
-router.put("/:sessionId", updateSession);
+router.put("/:sessionId", gameSessionValidationSchema, validationHandler, updateSession);
 
 export default router;

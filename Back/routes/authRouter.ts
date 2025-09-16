8321c6fdf4eb63f5398cb10c7f9106b76730f5ae
Yonatan Gorovetz <goro5357@gmail.com>
2025-09-16 14:22:42 +0300

    minor bug fixes regarding separation of user validation

--- Back/routes/authRouter.ts
+++ Back/routes/authRouter.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import { loginUser, registerUser } from "../controllers/authController";
-import { userValidationHandler, userValidationSchema } from "../middlewares/userValidation";
+import { userValidationSchema } from "../utils/validationSchemas/userSchema";
+import { validationHandler } from "../middlewares/validationHandler";
 
 const router = express.Router();
 
 router.post('/login', loginUser);
-router.post('/register', userValidationSchema, userValidationHandler, registerUser);
+router.post('/register', userValidationSchema, validationHandler, registerUser);
 
 export default router;

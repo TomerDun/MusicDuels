767b85adef0ddf8a2840d82e85a175e087ca5978
TomerDun <tomer@dunsky.name>
2025-09-19 03:00:39 +0300

    auth

--- Back/utils/authUtils.ts
+++ Back/utils/authUtils.ts
@@ -3,7 +3,7 @@ import { DbUser, User } from './types';
 import jwt from 'jsonwebtoken'
 import { UnauthorizedError } from './client-errors';
 
-function encryptPassword(password: string) {
+function hashPassword(password: string) {
     const saltRounds = 10;
     const salt = bcrypt.genSaltSync(saltRounds);
     return bcrypt.hashSync(password, salt);
@@ -14,16 +14,11 @@ function createToken(user: User):string {
     return token;
 }
 
-function sanitizeUser(dbUser: DbUser): User {
-    const { password, createdAt, updatedAt, ...user } = dbUser;
-    return user as User;
-}
-
 async function comparePass(pass: string, encryptPass: string) {
     if (!await bcrypt.compare(pass, encryptPass))
         throw new UnauthorizedError('Incorrect credentials')
 }
 
 export default {
-    encryptPassword, createToken, sanitizeUser, comparePass
+    hashPassword, createToken, comparePass
 }

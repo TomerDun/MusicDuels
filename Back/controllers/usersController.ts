62a0e5fa464e9a95abecb07594c63369de366d58
Yonatan Gorovetz <goro5357@gmail.com>
2025-09-21 16:09:26 +0300

    changed getUserProfile and updateUserProfile to getUser and update User, added db call to getUser

--- Back/controllers/usersController.ts
+++ Back/controllers/usersController.ts
@@ -1,16 +1,23 @@
 import { Request, Response } from "express";
 import StatusCode from "../utils/status-code";
+import { User } from "../db/models/user";
+import { ResourceNotFoundError } from "../utils/client-errors";
 
-export async function getUserProfile(req:Request, res:Response){
+export async function getUser(req:Request, res:Response){
     const userId = req.params.id;
-    res.status(StatusCode.OK);
-    res.send(`User Profile with id: ${userId} Retrieved Successfully`);
+    const user = await User.findByPk(userId);
+    
+    if (!user) {
+        throw new ResourceNotFoundError(`User with id: ${userId} not found`);
+    }
+    
+    res.status(StatusCode.OK).json(user);
 }
 
-export async function updateUserProfile(req:Request, res:Response){
+export async function updateUser(req:Request, res:Response){
     const userId = req.params.id;
     res.status(StatusCode.OK);
-    res.send(`User Profile with id: ${userId} Updated Successfully`);
+    res.send(`User with id: ${userId} Updated Successfully`);
 }
 
 export async function getUserStats(req:Request, res:Response){

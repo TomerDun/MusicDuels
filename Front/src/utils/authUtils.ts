e92810568493202887c4beb40beda9f5021de540
itaiglikman <itaig1998@gmail.com>
2025-09-23 13:37:17 +0300

    front support for image upload

--- Front/src/utils/authUtils.ts
+++ Front/src/utils/authUtils.ts
@@ -3,10 +3,10 @@
 import type { RegisterRequest } from "../types/AuthTypes";
 import { API_URL } from "./serverUtils";
 
-export async function onLogin(email:string, password:string) {
-    const body = JSON.stringify({email, password})
+export async function onLogin(email: string, password: string) {
+    const body = JSON.stringify({ email, password })
     try {
-        const res = await fetch(API_URL + '/auth/login', {body: body, method: 'POST', headers: {'Content-Type': 'application/json'}});
+        const res = await fetch(API_URL + '/auth/login', { body: body, method: 'POST', headers: { 'Content-Type': 'application/json' } });
         if (!res.ok) {
             const err = await res.json();
             throw new Error(err.message);
@@ -16,7 +16,7 @@ export async function onLogin(email:string, password:string) {
 
         localStorage.setItem('token', data.token);
         localStorage.setItem('userId', data.user.id);
-        
+
         return data;
     }
     catch (err) {
@@ -26,19 +26,37 @@ export async function onLogin(email:string, password:string) {
 }
 
 // TODO: add type for response from register (user type)
-// TODO: Change to form data after implementing image upload
-export async function onRegister(body:RegisterRequest) {    
+export async function onRegister(body: RegisterRequest) {
     try {
-        const res = await fetch(API_URL + '/auth/register', {body: JSON.stringify(body), method: 'POST', headers: {'Content-Type': 'application/json'}});
+        const formData = new FormData();
+
+        // Add all your form fields to FormData
+        formData.append('email', body.email);
+        formData.append('username', body.username);
+        formData.append('password', body.password);
+        if (body.profileImageFile)
+            formData.append('profileImageFile', body.profileImageFile);
+
+        const res = await fetch(API_URL + '/auth/register', {
+            body: formData,
+            method: 'POST',
+        });
         const data = await res.json();
 
         if (!res.ok) {
+            // handle existing email or username
+            if (data.message.errors[0].type.includes('unique'))
+                throw new Error(
+                    data.message.errors[0].path === 'username'
+                        ? 'Username already exists!'
+                        : 'Email already exists!'
+                )
             throw new Error(data.message);
         }
 
         localStorage.setItem('token', data.token);
         localStorage.setItem('userId', data.user.id);
-        
+
         return data;
     }
     catch (err) {

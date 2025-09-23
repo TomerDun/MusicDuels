f4f8183aed7706f66aa8da2075028e713fbf843e
TomerDun <tomer@dunsky.name>
2025-09-23 14:49:46 +0300

    added api url for production

--- Front/src/utils/serverUtils.ts
+++ Front/src/utils/serverUtils.ts
@@ -1,6 +1,7 @@
 // Utils related to backend server connection
 
-export const API_URL = 'http://localhost:8080'
+export const API_URL_DEV = 'http://localhost:8080'
+export const API_URL = 'https://musicduels.onrender.com' // PRODUCTION
 
 export function authHeaders() {
     const token = localStorage.getItem('token');

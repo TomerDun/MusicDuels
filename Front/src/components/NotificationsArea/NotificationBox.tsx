2407cf0858be43a71d6a754dc7c80689a62a917e
Yonatan Gorovetz <goro5357@gmail.com>
2025-09-24 21:44:19 +0300

    initial notification box component

--- Front/src/components/NotificationsArea/NotificationBox.tsx
+++ Front/src/components/NotificationsArea/NotificationBox.tsx
@@ -1,20 +1,122 @@
-export default function NotificationBox({notification} :{notification: any}) {
+import { observer } from "mobx-react-lite"
+import { userStore } from "../../stores/UserStore"
+import type { Notification } from "../../types/NotificationTypes";
+
+interface NotificationBoxProps{
+    notification:Notification,
+    handleDismiss:(event: React.MouseEvent<HTMLButtonElement>) => void,
+    handleShowResults:(event: React.MouseEvent<HTMLButtonElement>) => void,
+    handleGameAccept:(event: React.MouseEvent<HTMLButtonElement>) => void,
+    handleGameDecline:(event: React.MouseEvent<HTMLButtonElement>) => void
+}
+
+function NotificationBox({notification,handleDismiss,handleShowResults,handleGameAccept,handleGameDecline} :NotificationBoxProps) {
+
+    const activeUserId = userStore?.activeUser?.id;
 
     function notificationContent() {
-        switch(notification.type) {
-            case 'PENDING': {
-                return (
-                    <div>
-                        PENDING
-                    </div>
-                )
+        if(activeUserId === notification.senderId){
+            switch(notification.status) {
+                case 'PENDING': {
+                    return (
+                        <div>
+                            Waiting for {notification.recieverUsername} to respond.
+                        </div>
+                    )
+                }
+                case 'ACCEPTED': {
+                    return(
+                        <div>
+                            {notification.recieverUsername} has accepted the challenge!
+                        </div>
+                    )
+                }
+                case 'DECLINED': {
+                    return(
+                        <>
+                            <div>
+                                {notification.recieverUsername} has declined the challange.
+                            </div>
+                            <div id="button-section">
+                                <button onClick={handleDismiss}>
+                                    Dismiss
+                                </button>
+                            </div>
+                        </>
+                    )
+                }
+                case 'FINISHED': {
+                    return(
+                        <>
+                            <div>
+                                your challange with {notification.recieverUsername} has reached its conclusion!
+                            </div>
+                            <div id="button-section">
+                                <button onClick={handleShowResults}>
+                                    Show Game Results
+                                </button>
+                            </div>
+                        </>
+                    )
+                }
+            }
+        }
+        else{
+            switch(notification.status){
+                case 'PENDING':
+                    return(
+                        <>
+                            <div>
+                                {notification.senderUsername} challanged you to a game of {notification.gameType}!
+                            </div>
+                            <div id="button-section">
+                                <button onClick={handleGameAccept}>
+                                    ACCEPT
+                                </button>
+                                <button onClick={handleGameDecline}>
+                                    Decline
+                                </button>
+                            </div>
+                        </>
+                    )
+                case 'ACCEPTED':
+                    return(
+                        <div>
+                            Challange accepted!
+                        </div>
+                    )
+                case 'FINISHED':
+                    return(
+                        <>
+                            <div>
+                                your challange with {notification.senderUsername} has reached its conclusion!
+                            </div>
+                            <div id="button-section">
+                                <button onClick={handleShowResults}>
+                                    Show Game Results
+                                </button>
+                            </div>
+                        </>
+                    )
             }
         }
     }
 
     return (
         <div className="glass-container">
-            {notificationContent()}
+            <div id="top-section" className="glass-container">
+                <div id="game-type-container">
+                    {notification.gameType}
+                </div>
+                <div id="status-container">
+                    {notification.status}
+                </div>
+            </div>
+            <div id="content-container">
+                {notificationContent()}
+            </div>
         </div>
     )
 }
+
+export default observer(NotificationBox)

ce8ea6b14361d41f500f6c63ee5b7826375bc3ea
TomerDun <tomer@dunsky.name>
2025-09-25 13:36:04 +0300

    fixed user score bug

--- Back/utils/UserUtils.ts
+++ Back/utils/UserUtils.ts
@@ -5,7 +5,8 @@ import { ResourceNotFoundError } from "./client-errors";
 export async function updateUserTotalScore(userId:string, delta:number) {
     const user = await User.findByPk(userId);
     if (!user) throw new ResourceNotFoundError(userId);
-    user.totalScore += delta;
+    const newScore = Number(user.totalScore) + Number(delta);
+    user.totalScore = Number(newScore);
     if (user.totalScore < 0) {user.totalScore = 0};
     await user.save();
 

4412ead91cd4c2c26f78119c67fd8121d0219189
TomerDun <tomer@dunsky.name>
2025-09-27 18:40:39 +0300

    fixed image imports for inspiration modal

--- Front/src/components/GamesArea/InspirationSelector.tsx
+++ Front/src/components/GamesArea/InspirationSelector.tsx
@@ -1,3 +1,10 @@
+import BachImage from '../../assets/inpirations/bach.png'
+import MotzartImage from '../../assets/inpirations/mozart.png'
+import BeatlesImage from '../../assets/inpirations/beatles.png'
+import CharlieImage from '../../assets/inpirations/charlie.png'
+import DebussyImage from '../../assets/inpirations/debussy.png'
+import RandomImage from '../../assets/inpirations/random.png'
+
 
 export default function InspirationSelector({onPick}: {onPick: Function})  {
     return (
@@ -11,37 +18,37 @@ export default function InspirationSelector({onPick}: {onPick: Function})  {
 
                 <div onClick={() => onPick('mozart')} className="game-select-button !gap-2 interactive w-[30%]  from-orange-400/20 to-yellow-100/10">
                     {/* <div id="icon-box" className="icon-box bg-indigo-400/50"><IconPiano size={30}/></div> */}
-                    <img src="\src\assets\inpirations\mozart.png" alt=""className="max-h-70" />
+                    <img src={MotzartImage} alt="motzart" className="max-h-70" />
                     <h3 className="text-white font-bold text-lg" id="inspiration-title">Wolfgang Amadeus Mozart</h3>
                     <p className="text-white/70 text-center">Elegant, balanced, and melodically expressive</p>
                 </div>
                 <div onClick={() => onPick('bach')} className="game-select-button !gap-2 interactive w-[30%]  from-orange-400/20 to-yellow-100/10">
                     {/* <div id="icon-box" className="icon-box bg-amber-300/60"><IconMetronome /></div> */}
-                    <img src="\src\assets\inpirations\bach.png" alt="" className="max-h-70"/>
+                    <img src={BachImage} alt="bach" className="max-h-70"/>
                     <h3 className="text-white font-bold text-lg" id="inspiration-title">Johann Sebastian Bach</h3>
                     <p className="text-white/70 text-center">Intricate, structured, and harmonically rich</p>
                 </div>
                 <div onClick={() => onPick('deBussy')} className="game-select-button !gap-2 interactive w-[30%]  from-orange-400/20 to-yellow-100/10">
                     {/* <div id="icon-box" className="icon-box bg-teal-400/50"><IconEar /></div> */}
-                    <img src="\src\assets\inpirations\debussy.png" alt="" className="max-h-70"/>
+                    <img src={DebussyImage} alt="debussy" className="max-h-70"/>
                     <h3 className="text-white font-bold text-lg" id="inspiration-title">Claude DeBussy</h3>
                     <p className="text-white/70 text-center">Atmospheric, fluid, and impressionistic</p>
                 </div>
                 <div onClick={() => onPick('charlie-parker')} className="game-select-button !gap-2 interactive w-[30%] from-orange-400/20 to-yellow-100/10">
                     {/* <div id="icon-box" className="icon-box bg-teal-400/50"><IconEar /></div> */}
-                    <img src="\src\assets\inpirations\charlie.png" alt="" className="max-h-70"/>
+                    <img src={CharlieImage} alt="charlie-parker" className="max-h-70"/>
                     <h3 className="text-white font-bold text-lg" id="inspiration-title">Charlie Parker</h3>
                     <p className="text-white/70 text-center">Bold, virtuosic, and harmonically adventurous</p>
                 </div>
                 <div onClick={() => onPick('beatles')} className="game-select-button !gap-2 interactive w-[30%]  from-orange-400/20 to-yellow-100/10">
                     {/* <div id="icon-box" className="icon-box bg-teal-400/50"><IconEar /></div> */}
-                    <img src="\src\assets\inpirations\beatles.png" alt="" className="max-h-70"/>
+                    <img src={BeatlesImage} alt="the-beatles" className="max-h-70"/>
                     <h3 className="text-white font-bold text-lg" id="inspiration-title">The Beatles</h3>
                     <p className="text-white/70 text-center">Melodic, innovative, and genre-blending</p>
                 </div>
                 <div onClick={() => onPick('random')} className="game-select-button !gap-2 interactive w-[30%]  from-orange-400/20 to-yellow-100/10">
                     {/* <div id="icon-box" className="icon-box bg-teal-400/50"><IconEar /></div> */}
-                    <img src="\src\assets\inpirations\random.png" alt="" className="max-h-70"/>
+                    <img src={RandomImage} alt="random" className="max-h-70"/>
                     <h3 className="text-white font-bold text-lg" id="inspiration-title">Random</h3>
                     <p className="text-white/70 text-center">A random set of notes</p>
                 </div>
